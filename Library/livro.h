67da051247876c4666591777a132fdde2c196072
thales <thales@Ubuntu.myguest.virtualbox.org>
2023-04-28 18:35:53 -0300

    Documentação adicionada

--- Library/livro.h
+++ Library/livro.h
@@ -9,16 +9,30 @@
 typedef struct livro tLivro;
 typedef struct Biblioteca tBib;
 
-tLivro* Init_Books ();
+// Initializes a struct type tLivro pointer, which includes book`s contents as:
+// Title (STR), Author (STR), Edition (INT) and Number of Pages (INT)
+// OBS: It doesnt need any argument and it returns a type tLivro*
+tLivro *Init_Books();
 
-tBib* init_Bib ();
+// Initializes a struct type tBib pointer, which includes library`s contents as:
+// Double pointer for a struct type tLivro, which is a book`s catalog, Num of Books
+// OBS: It doesnt need any argument and in returns a type tBib*
+tBib *init_Bib();
 
-void preenche_Livros (tBib *B);
+// It fills the book`s content
+// Void function which requires only one tBib* argument
+void preenche_Livros(tBib *B);
 
-void print_Livros (tBib *B);
+// It prints the book`s content
+// Void function which requires only one tBib* argument
+void print_Livros(tBib *B);
 
-void free_Book (tLivro *L);
+// It frees the content of a book
+// Void function which requires only one tLivro* argument
+void free_Book(tLivro *L);
 
-void free_Bib (tBib *B);
+// It frees the content of a library
+// Void funciont which requires only one tBib* argument
+void free_Bib(tBib *B);
 
 #endif
